ad7bdd9882276a476276a1a5e62f6b9f386c230b
sumagnadas <sumagnadas@gmail.com>
2025-08-15 01:40:29 +0530

    Add some ground texture.

--- src/game/main.js
+++ src/game/main.js
@@ -2,7 +2,7 @@ import { AUTO, Game } from 'phaser';
 import envObjects from './objects';
 
 const StartGame = (parent) => {
-    var groundLevel = 500, playerOffsetY = 50;
+    var groundLevel = 500, playerOffsetY = 100;
     var posPresets = { 'top': { y: 100 }, 'middle': { y: 250 }, 'bottom': { y: groundLevel } }
     // var currentWidth = 800;
     var currentWidth = window.innerWidth;
@@ -21,6 +21,9 @@ const StartGame = (parent) => {
                 debug: false
             }
         },
+        audio: {
+            noAudio: true,
+        },
         // scale: {
         //     mode: Phaser.Scale.FIT,
         //     autoCenter: Phaser.Scale.CENTER_BOTH
@@ -35,7 +38,8 @@ const StartGame = (parent) => {
 
     function preload() {
         this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
+        this.load.image('ground_top', 'assets/ground_top.png');
+        this.load.image('ground_bottom', 'assets/ground_bottom.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
         this.load.image('bench', 'assets/bench.png');
@@ -45,7 +49,7 @@ const StartGame = (parent) => {
         );
     }
 
-    var player, ground, sprites = [];
+    var player, ground_top, ground_bottom, sprites = [];
     function create() {
         var camera = this.cameras.main;
         for (let index = 0; index < envObjects.length; index++) {
@@ -67,14 +71,21 @@ const StartGame = (parent) => {
             console.log(`Display: ${sprite.displayWidth}\nactual: ${sprite.width}`)
         }
         player = this.physics.add.sprite(0, groundLevel - 24, 'dude');
-        ground = this.add.tileSprite(-100, groundLevel, currentWidth, 64, 'sky');
+        ground_top = this.add.tileSprite(-100, groundLevel, currentWidth, 64, 'ground_top');
+        ground_bottom = this.add.tileSprite(-100, groundLevel + 64, currentWidth, currentWidth / 2 - playerOffsetY, 'ground_bottom');
 
-        this.physics.add.existing(ground);
+        this.physics.add.existing(ground_top);
+        this.physics.add.existing(ground_bottom);
 
-        ground.setOrigin(0, 0);
-        ground.body.immovable = true;
-        ground.body.allowGravity = false;
-        ground.body.setFriction(0, 0);
+        ground_top.setOrigin(0, 0);
+        ground_top.body.immovable = true;
+        ground_top.body.allowGravity = false;
+        ground_top.body.setFriction(0, 0);
+
+        ground_bottom.setOrigin(0, 0);
+        ground_bottom.body.immovable = true;
+        ground_bottom.body.allowGravity = false;
+        ground_bottom.body.setFriction(0, 0);
 
         player.setBounce(0.2);
 
@@ -100,24 +111,29 @@ const StartGame = (parent) => {
             frameRate: 10,
             repeat: -1
         });
-        this.physics.add.collider(player, ground);
+        this.physics.add.collider(player, ground_top);
     }
     var cursors;
     function update() {
         cursors = this.input.keyboard.createCursorKeys();
         if (cursors.left.isDown) {
             player.setVelocityX(-160);
-            ground.body.setVelocityX(-160);
+            ground_top.body.setVelocityX(-160);
+            ground_bottom.body.setVelocityX(-160);
+            ground_top.tilePositionX -= 2.7;
             player.anims.play('left', true);
         }
         else if (cursors.right.isDown) {
             player.setVelocityX(160);
-            ground.body.setVelocityX(160);
+            ground_top.body.setVelocityX(160);
+            ground_top.tilePositionX += 2.7;
+            ground_bottom.body.setVelocityX(160);
             player.anims.play('right', true);
         }
         else {
             player.setVelocityX(0);
-            ground.body.setVelocityX(0);
+            ground_top.body.setVelocityX(0);
+            ground_bottom.body.setVelocityX(0);
             player.anims.play('turn');
         }
 
