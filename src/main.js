dad639649a3aa523f8ac3b7120910f2ddcf46ade
sumagnadas <sumagnadas@gmail.com>
2025-08-18 17:36:05 +0530

    [fix] Fixed the canvas size not resizing when the windows changes orientation or size.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import StartGame from './game/main';
 
+document.addEventListener('resize', () => { StartGame('game-container') })
 document.addEventListener('DOMContentLoaded', () => {
 
     StartGame('game-container');
