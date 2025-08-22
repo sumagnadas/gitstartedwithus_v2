3a60abd858edcd9aff6539ba89ac8ccc87342731
sumagnadas <sumagnadas@gmail.com>
2025-08-22 18:31:36 +0530

    [refactor] Clean up objects file.

--- src/game/objects/2025/objects.js
+++ src/game/objects/2025/objects.js
@@ -1,20 +1,4 @@
 const objects = import.meta.glob('./object_files/*.js');
-var envObjects = [
-    {
-        id: 'star',
-        x: 1,
-        y: 100, height: 'bottom'
-    },
-    {
-        id: 'bomb',
-        x: 200,
-        y: 100, height: 'middle'
-    },
-    {
-        id: 'star', height: 'bottom'
-    },
-    { id: 'bench', height: 'bottom', name: 'Sumagna Das' },
-    { id: 'bench', height: 'top', name: 'Sumagna Das' },
-];
+var envObjects = [];
 Object.keys(objects).forEach((key) => { var promise = objects[key](); promise.then((data) => { envObjects.push(data.default) }) });
 export default envObjects;
