68a1045315dba69b082e8109ccce3793625cb549
mandeeprewar45-boop <mandeeprewar45@gmail.com>
2025-08-23 11:54:54 +0530

    Create mandeepreawr.js

---
+++ src/game/objects/mandeepreawr.js
@@ -0,0 +1,8 @@
+const config = {
+    id: 'anywhere_door',
+    name: 'Mandeep Choudhary',
+    color: '#ffaa55',
+    githubId: 'mandeeprewar45-boop',
+    // z_pos: 'behind'
+};
+export default config;
